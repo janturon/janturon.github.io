8abe49e8da92b54dd6100f990fbb5086228868e2
Jan Turon <janturon@email.cz>
2021-04-11 11:49:56 +0200

    xhr

--- src/xhr.js
+++ src/xhr.js
@@ -1,16 +1,16 @@
-const XHR = (server, get, post, progress) => new Promise((resolve,reject) => { let
+const XHR = (server, get, post, Pfn, Efn) => new Promise((Yes,No) => { let
 	E = encodeURIComponent,
-	P = obj => Object.keys(obj).map(key => E(key) + '=' + E(obj[key])).join('&'),
-	O = "object",
+	P = o => Object.keys(o).map(k => E(k)+'='+E(o[k])).join('&'),
+  O = s => e => Pfn(e.loaded, e.total, s)
 	X = new XMLHttpRequest()
-	X.onload = _ => resolve(X.response, X.getAllResponseHeaders())
-	X.onerror = reject
-	if(progress) X.onprogress = e => progress(e.loaded, e.total)
-	if(typeof get ==O && get!==null) get = P(get)
-	if(typeof post==O &&post!==null) post= P(post)
+	X.onload = _=> Yes({response:X.response, headers:X.getAllResponseHeaders())
+	X.onerror = No
+	Pfn && (X.onprogress=O(0), X.upload.onprogress=O(1))
+	if(typeof get =="object" && get!==null) get = P(get)
+	if(typeof post=="object" &&post!==null) post= P(post)
 	if(get) server+= "?"+get
 	X.open(post?"POST":"GET", server)
 	if(post) X.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-	X.withCredentials = 1
+	if(Efn) X = Efn(X)
 	X.send(post)
 })

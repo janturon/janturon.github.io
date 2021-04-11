e497223fd2c168020c354ffc900c92fd1a6259b7
Jan Turon <janturon@email.cz>
2021-04-11 11:54:45 +0200

    xhr

--- src/xhr.js
+++ src/xhr.js
@@ -1,9 +1,9 @@
 const XHR = (server, get, post, Pfn, Efn) => new Promise((Yes,No) => { let
 	E = encodeURIComponent,
 	P = o => Object.keys(o).map(k => E(k)+'='+E(o[k])).join('&'),
-  O = s => e => Pfn(e.loaded, e.total, s)
+  O = s => e => Pfn(e.loaded,e.total,s)
 	X = new XMLHttpRequest()
-	X.onload = _=> Yes({response:X.response, headers:X.getAllResponseHeaders())
+	X.onload = _ => Yes({response:X.response, headers:X.getAllResponseHeaders()})
 	X.onerror = No
 	Pfn && (X.onprogress=O(0), X.upload.onprogress=O(1))
 	if(typeof get =="object" && get!==null) get = P(get)
